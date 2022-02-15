2db52cb95e85d53cf2ae4ee65e01ab0bde983dbe
seitosugino <0jb4f61k3853v2f@gmail.com>
2022-02-15 23:19:21 +0900

    基本の章まで

--- app/src/test/java/com/example/tutoriaial/CalculatorTest.kt
+++ app/src/test/java/com/example/tutoriaial/CalculatorTest.kt
@@ -2,13 +2,18 @@ package com.example.tutoriaial
 
 import org.junit.Assert.*
 import org.assertj.core.api.Assertions.*
+import org.junit.Before
 import org.junit.Test
 
 class CalculatorTest {
+    lateinit var sut: Calculator
 
+    @Before
+    fun setUp() {
+        sut = Calculator()
+    }
     @Test
     fun multiplyで2と3を掛けたら6が取得できる() {
-        val sut = Calculator()
         val actual = sut.multiply(2,3)
         val expected = 6
 
@@ -17,7 +22,6 @@ class CalculatorTest {
 
     @Test
     fun multiplyで0と3を掛けたら0が取得できる() {
-        val sut = Calculator()
         val actual = sut.multiply(0,3)
         val expected = 0
 
@@ -26,16 +30,14 @@ class CalculatorTest {
 
     @Test
     fun divideで6を2で割った結果を取得できる() {
-        val sut = Calculator()
         val actual = sut.divide(6,2)
-        val expected = 3
+        val expected = 3.0
 
         assertThat(actual).isEqualTo(expected)
     }
 
     @Test
     fun divideで3を2で割った結果を取得できる() {
-        val sut = Calculator()
         val actual = sut.divide(3,2)
         val expected = 1.5
 
@@ -44,7 +46,6 @@ class CalculatorTest {
 
     @Test(expected = IllegalArgumentException::class)
     fun divideで3を0で割るとIllegalArgumentExceptionが送出される() {
-        val sut = Calculator()
         val actual = sut.divide(3,0)
     }
 
